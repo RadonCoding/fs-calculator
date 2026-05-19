2a93abf1d41d51f7fa747fdc0722c443b8114783
RadonCoding <henry.welch@reborn.com>
2026-05-19 21:04:46 +0300

    Playwright GitHub CI debugging

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/evaluate": `http://localhost:${process.env.PORT || 5001}`,
+      "/evaluate": `http://127.0.0.1:${process.env.PORT || 5001}`,
     },
   },
   build: {
